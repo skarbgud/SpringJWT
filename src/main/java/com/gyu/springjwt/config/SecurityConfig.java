b2e1f16fd3fdb5f0403de4e3375c7094f581d7e4
GyuHyeong Nam <sjarbgud@gmail.com>
2022-08-29 00:38:03 +0900

    Initial commit

---
+++ src/main/java/com/gyu/springjwt/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.gyu.springjwt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/api/hello").permitAll() // api/hello에 대한 요청은 인증없이 허용
+                .anyRequest().authenticated(); // 나머지 요청들은 인증을 해야한다.
+    }
+}
